56ba3548f09d026a50fd71973ccde197f22bb5db
JerryVW <jerryvw72@gmail.com>
2020-02-07 13:13:01 -0500

    Worked on javascript functionality for the game, made a few changes to HTML and CSS

---
+++ script.js
@@ -0,0 +1,78 @@
+let Rock = document.getElementById("rock").addEventListener("click", compareChoices);
+let Paper = document.getElementById("paper").addEventListener("click", compareChoices);
+let Scissors = document.getElementById("scissors").addEventListener("click", compareChoices);
+
+let playerChoice;
+let computerChoice;
+
+
+function compareChoices(e) {
+    e.preventDefault();
+    playerChoice = playerSelection();
+    computerChoice = computerSelection();
+    switch (playerChoice + " " + computerChoice) {
+        case "Rock Scissors":
+        case "Paper Rock":
+        case "Scissors Paper":
+            won();
+            break;
+
+        case "Rock Paper":
+        case "Paper Scissors":
+        case "Scissors Rock":
+            console.log("Player Lost!!");
+            break;
+
+        case "Rock Rock":
+        case "Paper Paper":
+        case "Scissors Scissors":
+            console.log("It is a draw!!");
+            break;
+    }
+    resetGame();
+} 
+
+
+function playerSelection() {
+    playerChose = this.innerHTML;
+    return playerChose;
+}
+
+function computerSelection() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomSelection = Math.floor(Math.random() * 3);
+    return choices[randomSelection];
+}
+
+function won(compareChoices) {
+    if(playerSelection == "Rock"){
+        Rock.classList.add("rock-btn-won");
+    } else if(playerSelection == "Paper") {
+        Paper.classList.add("paper-btn-won");
+    } else if(playerSelection == "Scissors") {
+        Scissors.classList.add("scissors-btn-won");
+    }
+    console.log("Player Wins!!");
+}
+
+function lost() {
+
+}
+
+function draw() {
+
+}
+
+
+function resetGame(e) {
+    const inputSpace = document.getElementById("reset-game");
+    const button = document.createElement("button");
+    button.type = "reset";
+    button.className = "play-again";
+    button.textContent = "Play Again?";
+    inputSpace.appendChild(button);
+    button.addEventListener("click", function() {
+        window.location.reload();
+    });
+    return inputSpace;
+}
