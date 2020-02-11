02d24195d29927df9d7786c790076945b1a8d133
JerryVW <jerryvw72@gmail.com>
2020-02-11 12:29:39 -0500

    Worked on getting score to show up on the screen

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ let Scissors = document.getElementById("scissors").addEventListener("click", com
 
 let playerScore = 0;
 let computerScore = 0;
+let tie = 0;
+const playerArray = JSON.parse(localStorage.getItem("player_score")) || [];
+const computerArray = JSON.parse(localStorage.getItem("computer_score")) || [];
+const tieArray = JSON.parse(localStorage.getItem("tie_score")) || [];
 let playerChoice;
 let computerChoice;
+
 
 function computerSelection() {
     const choices = ["Rock", "Paper", "Scissors"];
@@ -18,53 +23,79 @@ function compareChoices(e) {
     e.preventDefault();
     playerChoice = e.target.textContent;
     computerChoice = computerSelection();
-    console.log(playerChoice + " " + computerChoice);
     switch (playerChoice + " " + computerChoice) {
         case "Rock Scissors":
         case "Paper Rock":
         case "Scissors Paper":
-            won();
+            won(playerChoice);
             break;
 
         case "Rock Paper":
         case "Paper Scissors":
         case "Scissors Rock":
             lost();
-            console.log("Player Lost!!");
             break;
 
         case "Rock Rock":
         case "Paper Paper":
         case "Scissors Scissors":
-            console.log("It is a draw!!");
+            draw();
             break;
     }
+    disableBtn();
     resetGame();
-    return playerChoice;
+    quitGame();
 } 
 
+function disableBtn() {
+    document.getElementById("rock").disabled = true;
+    document.getElementById("paper").disabled = true;
+    document.getElementById("scissors").disabled = true;
+}
 
 
-function won(playerChoice) {
+function won() {
+    playerScore++;
+    document.getElementById("player_score").innerHTML = playerScore;
+    playerArray.push(playerScore++);
+    localStorage.setItem("player_score", JSON.stringify(playerArray));
     if(playerChoice == "Rock"){
         document.getElementById("rock").classList.add("rock-btn-won");
+        document.getElementById("game-outcome").innerHTML = "Player Wins!! You chose Rock against Computers choice of Scissors!"
     } else if(playerChoice == "Paper") {
         document.getElementById("paper").classList.add("paper-btn-won");
+        document.getElementById("game-outcome").innerHTML = "Player Wins!! You chose Paper against Computers choice of Rock!"
     } else if(playerChoice == "Scissors") {
         document.getElementById("scissors").classList.add("scissors-btn-won");
+        document.getElementById("game-outcome").innerHTML = "Player Wins!! You chose Scissors against Computers choice of Paper!"
     }
-    console.log("Player Wins!!");
 }
 
 function lost() {
+    computerScore++;
+    document.getElementById("computer_score").innerHTML = computerScore;
+    computerArray.push(computerScore++);
+    localStorage.setItem("computer_score", JSON.stringify(computerArray));
     if(computerChoice == "Paper"){
         document.getElementById("rock").classList.add("rock-btn-lost");
+        document.getElementById("game-outcome").innerHTML = "Computer Wins!! The Computer chose Paper against Your choice of Rock!"
     } else if(computerChoice == "Scissors") {
         document.getElementById("paper").classList.add("paper-btn-lost");
+        document.getElementById("game-outcome").innerHTML = "Computer Wins!! The Computer chose Scissors against Your choice of Paper!"
     } else if(computerChoice == "Rock") {
         document.getElementById("scissors").classList.add("scissors-btn-lost");
+        document.getElementById("game-outcome").innerHTML = "Computer Wins!! The Computer chose Rock against Your choice of Scissors!"
     }
-    console.log("Player Lost!!");
+    
+}
+
+function draw() {
+    tie++;
+    document.getElementById("tie_score").innerHTML = tie;
+    tieArray.push(tie);
+    localStorage.setItem("tie_score", JSON.stringify(tieArray));
+    tieArray.push(tie++);
+    document.getElementById("game-outcome").innerHTML = "The game is a Draw!! You both chose the same thing!!"
 }
 
 function resetGame(e) {
@@ -76,6 +107,20 @@ function resetGame(e) {
     inputSpace.appendChild(button);
     button.addEventListener("click", function() {
         window.location.reload();
+        document.querySelectorAll("button").disabled = false;
     });
     return inputSpace;
+}
+
+function quitGame() {
+    const quitBtn = document.getElementById("quit-game");
+    const button = document.createElement("button");
+    button.type = "reset";
+    button.className = "quit-game";
+    button.textContent = "Quit Game";
+    quitBtn.appendChild(button);
+    button.addEventListener("click", function() {
+        document.getElementById("game-outcome").innerHTML = "Thank You For Playing!!"
+    });
+    return quitBtn;
 }
